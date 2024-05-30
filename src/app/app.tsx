c58c714e9d32f0f7f8bb6cd1968d4c4b457f1b46
bithighlander <bithighlander@gmail.com>
2024-05-30 16:36:25 -0600

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,49 @@
+'use client';
+import { Box, Button, Container, Flex, Heading, HStack, IconButton, Text, VStack } from "@chakra-ui/react";
+import {
+  Amount,
+  Asset,
+  Assets,
+  Basic,
+  Classic,
+  Pioneer,
+  Portfolio,
+  Swap,
+  Transfer,
+} from '@coinmasters/pioneer-lib';
+import { usePioneer } from "@coinmasters/pioneer-react";
+import { useEffect, useState } from 'react';
+import { FaBriefcase, FaCoins } from 'react-icons/fa';
+import { LP_GRID_ITEMS } from "lp-items";
+import Header from "../components/Header";
+import { useOnStartApp } from "../utils/onStart";
+
+export default function App() {
+  const onStartApp = useOnStartApp();
+  const { state } = usePioneer();
+  const { app } = state;
+  const [selectedAsset, setSelectedAsset] = useState({});
+  const [activeTab, setActiveTab] = useState('portfolio');
+
+  useEffect(() => {
+    onStartApp();
+  }, []);
+
+  useEffect(() => {
+    if (app && app.assetContext) setSelectedAsset(app.assetContext);
+  }, [app, app?.assetContext]);
+
+  const onClose = () => {
+    //console.log("onClose")
+  };
+
+  const onSelect = (asset: any) => {
+    //console.log("onSelect: ", asset)
+  }
+
+  return (
+      <>
+        <Classic usePioneer={usePioneer}></Classic>
+      </>
+  );
+}
